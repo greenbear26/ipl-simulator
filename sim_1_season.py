c440e3cc31dd52a21363fd01da930f43df5b48b8
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-05-14 11:24:04 -0400

    Made code more efficient

--- sim_1_season.py
+++ sim_1_season.py
@@ -1,5 +1,5 @@
-import simfunctions
+import sim_functions
 
 # Print final standings
-for team in reversed(simfunctions.simulate_season()):
+for team in reversed(sim_functions.simulate_season()):
     print(f'{team["name"]} Points: {team["points"]} NRR: {team["nrr"]:.3f}')
---
+++ sim_multiple_seasons.py
@@ -0,0 +1,36 @@
+import sim_functions
+import sys
+
+simulations = int(
+    sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1].isdigit() else int(
+        input("How many simulations? "))
+print(f"{simulations} simulations")
+
+overall_team_score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+counter = 0
+
+while counter < simulations:
+    team_stats = sim_functions.get_team_stats()
+    team_stats_sorted = sim_functions.simulate_season()
+
+    for i in range(0, len(team_stats_sorted)):
+        team_index = sim_functions.find_team_index(
+            team_stats_sorted[i]["name"], team_stats)
+        overall_team_score[team_index] += i + 1
+    counter += 1
+
+# Print final results
+team_stats = sim_functions.get_team_stats()
+
+team_stats_sorted = sorted([(i, j)
+                            for i, j in zip(team_stats, overall_team_score)],
+                           key=lambda x: x[1])
+
+print("Average Team Positioning")
+for i in range(len(overall_team_score) - 1, -1, -1):
+    team, score = team_stats_sorted[i]
+    # print(f'{team["name"]} Total Standing: {score}')
+    print(
+        f'{team["name"]} Average Standing: {score/simulations}'
+    )
