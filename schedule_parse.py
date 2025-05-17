fb7fe08fc2d2bad9ecec41f6cb103317ca164bc7
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-05-17 16:38:08 -0400

    Updated standings and date comparison logic

--- schedule_parse.py
+++ schedule_parse.py
@@ -24,12 +24,13 @@ def get_remaining_schedule():
             if counter == 0:
                 counter += 1
                 continue
-            date = datetime.strptime(row[2], "%d/%m/%Y %H:%M")
-            # print(date)
+            date = datetime.strptime(row[2], "%d/%m/%Y %H:%M") + timedelta(hours
+                                                                           = 4)
             present = datetime.now()
-            if (date.date() >= present.date()):
+            # print(f"Match: {date} Now: {present} Match Later than now: {date>present}")
+            if (date >= present):
                 if row[4] in team_names and row[5] in team_names:
                     remaining_schedule.append((team_names[row[4]],
                                                team_names[row[5]]))
+    print(remaining_schedule)
     return remaining_schedule
-
--- sim_functions.py
+++ sim_functions.py
@@ -35,12 +35,12 @@ def simulate_match(team1_name, team2_name, team_stats):
 
 def get_team_stats():
     TEAM_STATS = [
+        dict(name="RCB", matches=12, wins=8, losses=3, nrr=0.482, points=17),
         dict(name="GT", matches=11, wins=8, losses=3, nrr=0.793, points=16),
-        dict(name="RCB", matches=11, wins=8, losses=3, nrr=0.482, points=16),
         dict(name="PBKS", matches=11, wins=7, losses=3, nrr=0.376, points=15),
         dict(name="MI", matches=12, wins=7, losses=5, nrr=1.156, points=14),
         dict(name="DC", matches=11, wins=6, losses=4, nrr=0.362, points=13),
-        dict(name="KKR", matches=12, wins=5, losses=6, nrr=0.193, points=11),
+        dict(name="KKR", matches=13, wins=5, losses=6, nrr=0.193, points=12),
         dict(name="LSG", matches=11, wins=5, losses=6, nrr=-0.469, points=10),
         dict(name="SRH", matches=11, wins=3, losses=7, nrr=-1.192, points=7),
         dict(name="RR", matches=12, wins=3, losses=9, nrr=-0.718, points=6),
